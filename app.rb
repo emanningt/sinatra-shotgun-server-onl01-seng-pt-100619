0c53d4c4c937fbeef8c66e5e8e668fd97b0629e2
Eric Manning <emanningmusic@yahoo.com>
2020-01-22 15:42:55 -0800

    udate

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    "Welcome to your app!!!! And this is a test"
-  end
+ get "/" do
+   erb :index
+ end
 
 end
--- config.ru
+++ config.ru
@@ -1,7 +1,3 @@
 require './config/environment'
 
-get '/' do
-  "Started my server using Shotgun!"
-end
-
 run App
